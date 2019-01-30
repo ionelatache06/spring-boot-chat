441cb5bf70decb304ae24491979c370b3ab0c4b2
Ionela Tache <ionelatache06@yahoo.com>
2019-01-30 19:02:47 +0200

    commit

---
+++ src/main/java/com/example/thespringbootchat/TheSpringBootChatApplication.java
@@ -0,0 +1,21 @@
+package com.example.thespringbootchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TheSpringBootChatApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TheSpringBootChatApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSpringBootChatApplication.class, args);
+	}
+
+}
+
